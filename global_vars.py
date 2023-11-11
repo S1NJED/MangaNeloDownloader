7e53c9306365612b04cd2285ae2deba9480542d9
sinjed <oooguuh@gmail.com>
2023-11-11 12:34:59 +0100

    Adding global_vars.py file
    Signed-off-by: sinjed <oooguuh@gmail.com>

---
+++ global_vars.py
@@ -0,0 +1,8 @@
+# Change the User-Agent here if this don't work for you
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1'
+
+# Waiting time between each images downloaded
+MS = 0.2
+
+# Download path, can be relative or absolute
+DOWNLOAD_PATH = "D:\MangaNeloScans"
