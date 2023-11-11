ce083b0b7298ff404a5875a28818a51675cf6c12
sinjed <oooguuh@gmail.com>
2023-11-11 18:12:21 +0100

    Edit waiting time
    Signed-off-by: sinjed <oooguuh@gmail.com>

--- global_vars.py
+++ global_vars.py
@@ -2,7 +2,7 @@
 USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1'
 
 # Waiting time between each images downloaded
-MS = 0.2
+MS = 0.5
 
 # Download path, can be relative or absolute
 DOWNLOAD_PATH = "D:\MangaNeloScans"
